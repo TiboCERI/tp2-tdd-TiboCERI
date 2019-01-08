218ab79858b0c2b78db7c2bfabff9d7809015577
Chastelliere Thibaut <uapv1502382@ge-st3-101.univ-avignon.fr>
2019-01-08 14:51:19 +0100

    Exo1

--- tp2-tdd-TiboCERI/Exo1/src/FizzBuzz.java
+++ tp2-tdd-TiboCERI/Exo1/src/FizzBuzz.java
@@ -1,4 +1,7 @@
-
-public class FizzBuzz {
-
+public class FizzBuzz
+{
+	public String getResult(int x)
+	{
+		return(x == 0 ? Integer.toString(0) : (x%3 == 0 && x%5 == 0) ? "FizzBuzz" : x%3 == 0 ? "Fizz" : x%5 == 0 ? "Buzz" : Integer.toString(x)) ;
+	}
 }
--- tp2-tdd-TiboCERI/Exo1/src/FizzBuzzTest.java
+++ tp2-tdd-TiboCERI/Exo1/src/FizzBuzzTest.java
@@ -2,11 +2,64 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
 public class FizzBuzzTest {
 
+private FizzBuzz fizz ;
+	
+
+	void initialize()
+	{
+		this.fizz = new FizzBuzz() ;
+	}
+	
 	@Test
-	public void ShouldReturnUnWhen1() {
-		FizzBuzz fb = new FizzBuzz();
+	void shouldRetourneUnWhen1Test() 
+	{
+		assertTrue(this.fizz.getResult(1).equals("1")) ;
+	}
+	
+	
+	@Test
+	void shouldRetourneDeuxWhen2Test()
+	{
+		assertTrue(fizz.getResult(2).equals("2")) ;
+	}
+	
+	@Test
+	void shouldRetourneFizzWhen3Test()
+	{
+		assertTrue(fizz.getResult(3).equals("Fizz")) ;
+	}
+	
+	@Test
+	void shouldRetourneBuzzWhen5Test()
+	{
+		assertTrue(fizz.getResult(5).equals("Buzz")) ;
+	}
+	
+	@Test
+	void shouldRetouneFizzwhen9Test()
+	{
+		assertTrue(fizz.getResult(9).equals("Fizz")) ;
+	}
+	
+	@Test
+	void shouldRetouneBuzzWhen10Test()
+	{
+		assertTrue(fizz.getResult(10).equals("Buzz")) ;
+	}
+	
+	@Test
+	void shouldRetourneFizzBuzzWhen15Test()
+	{
+		assertTrue(fizz.getResult(15).equals("FizzBuzz")) ;
+	}
+	
+	@Test
+	void shouldRetounrStringWhen0orOtherNumberTest()
+	{
+		assertTrue(fizz.getResult(0).equals("0")) ;
 	}
 
 }
